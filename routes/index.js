9ad9384c16b8d57924a46a0ee5dc1879a4caacaa
Maxim <max.degtyareff@yandex.ru>
2016-08-23 17:58:11 -0700

    5 Fix

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 //Основной заголовок
 
 router.get('/', function (req,res){
-    res.render('index/' + page);
+    res.render('index/index');
 });
 
 module.exports = router;
