4fef5bfef76a5c0b6b107bc60dccb5a3e8a3e491
Maxim <max.degtyareff@yandex.ru>
2016-09-08 17:34:12 -0700

    Release from 08

--- app.js
+++ app.js
@@ -3,16 +3,16 @@ var express = require('express'),
     bodyParser = require('body-parser'),
     path = require('path'),
     errors = require('./midd/error.js'),
-    logs = require('./midd/logger.js');
+    logs = require('./midd/logger.js'),
+	dotenv = require('dotenv'),
+    path = require('path');
 
 
 
 //Environment
 
-//require('dotenv').config();
-require('dotenv/config');
-//var reg = require('./models/register');
-
+	dotenv.load();
+	
 //Routes include
 
 var indexRouter = require('./routes/index'),
@@ -23,34 +23,33 @@ var indexRouter = require('./routes/index'),
 /*Disable*/
 app.disable('x-powered-by');
 
-/*Set*/
 
+/*Set*/
 app.set('view engine', 'pug');
-app.set('views', __dirname + '/pages');
+app.set('views', path.join(__dirname, '/pages'));
 app.set('view cache');
 
-/*Middleware*/
 
+/*Middleware*/
 app.use(bodyParser.urlencoded({extended: true})); //BP
-app.use(express.static(__dirname + '/static')); //Static Files
+app.use(express.static(path.join(__dirname, '/static'))); //Static Files
 app.use(logs());
 
 
-//Здесь будут роуты
-
+/*Здесь будут роуты*/
 app.use('/', indexRouter);
 app.use('/cab', cabRouter);
 
 //Прослушивание и обработка ошибок
 
-
 var port = process.env.PORT || 3000;
 
 app.listen(port, function(){
-    console.log('Server has started');
-    console.log(process.env.LOL);
+    console.log('Listening on port: ' + port);
+    //console.log(process.env.LOL);
 });
 
 //errors middleware
 app.use(errors());
+
 module.exports = app;
